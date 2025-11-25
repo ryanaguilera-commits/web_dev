e14496c30eb6bf2c3ff22906578c667534edbc1e
ryanaguilera <ryan.aguilera@ucas-edu.net>
2025-11-25 14:41:48 -0700

    mkwenfneronier

---
+++ notes.js
@@ -0,0 +1,26 @@
+
+
+function hello(){
+
+    document.getElementById("title").innerHTML = "Hello World"
+}
+
+function enter(){
+    document.getElementById("btn").style.color = "white"
+    document.getElementById("btn").style.backgroundColor = "blue"
+}
+function click(){
+    document.getElementById("btn").style.backgroundColor = "red"
+}
+
+function out(){
+    document.getElementById("btn").style.color = "blue"
+    document.getElementById("btn").style.backgroundColor = "auqa"
+}
+
+let images = ["https://www.ufc.com/images/styles/athlete_bio_full_body/s3/2018-10/JOHNSON_DEMETRIOUS_L.png?VersionId=HRDdP7nmVjZwEyiRKdzDsVlwTPEUIfm_&itok=78m5FpiX"]
+
+
+function change(){
+    document.getElementById("Image").src = "https://www.ufc.com/images/styles/athlete_bio_full_body/s3/2018-10/JOHNSON_DEMETRIOUS_L.png?VersionId=HRDdP7nmVjZwEyiRKdzDsVlwTPEUIfm_&itok=78m5FpiX"
+}
